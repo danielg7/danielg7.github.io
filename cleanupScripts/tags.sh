fe028916e573bf8aaac5e4559df3245f1105fae1
danielg7 <danielg7@gmail.com>
2017-02-03 19:11:18 -0700

    Removing old cruft

--- cleanupScripts/tags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd _posts/blog
-
-for file in *; do
-  sed -i 's,http://www.chipoglesby.com/tag/,http://www.chipoglesby.com/tag/#,g' $file
-done
--- cleanupScripts/weightLoss.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd _posts/blog || exit
-
-for file in *; do
-  sed -i 's/weightloss/- weightloss/g' "$file"
-done
